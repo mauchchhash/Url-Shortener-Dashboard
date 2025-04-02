099601867e7ae5d5f210c95c0537a7c6e61240fb
Mahim Anjum Uchchhwas <51711462+mauchchhash@users.noreply.github.com>
2025-04-02 18:45:36 +0600

    Refactor: removed unused route middlewares

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,29 +1,12 @@
 import useAuthStore from '@/store/authStore'
 import { createRouter, createWebHistory, type RouteLocationNormalized } from 'vue-router'
 
-async function validateAuthentication() {
-  const authStore = useAuthStore()
-  if (authStore.accessToken == null && (await authStore.refreshToken()) == false) {
-    return { name: 'login' }
-  }
-  return true
-}
-
-async function validateGuest(to: RouteLocationNormalized, from: RouteLocationNormalized) {
-  const authStore = useAuthStore()
-  // console.log({ to, from })
-  if (authStore.accessToken || (await authStore.refreshToken()) == true)
-    return { name: 'dashboard' }
-  return true
-}
-
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
       name: 'home',
-      // beforeEnter: [validateAuthentication],
       meta: { requiresAuth: true },
       redirect: 'dashboard',
       children: [
@@ -37,20 +20,22 @@ const router = createRouter({
     {
       path: '/login',
       name: 'login',
-      // beforeEnter: [validateGuest],
       meta: { requiresGuest: true },
       component: () => import('../views/auth/LoginView.vue'),
     },
     {
       path: '/register',
       name: 'register',
-      // beforeEnter: [validateGuest],
       meta: { requiresGuest: true },
       component: () => import('../views/auth/RegisterView.vue'),
     },
   ],
 })
 router.beforeEach(async (to: RouteLocationNormalized, from: RouteLocationNormalized) => {
+  // console.log('--------------------------- Route ---------------------------')
+  // console.log({ to })
+  // console.log({ from })
+
   if (to.meta.requiresAuth) {
     const authStore = useAuthStore()
     if (authStore.accessToken == null && (await authStore.refreshToken()) == false) {
@@ -65,9 +50,6 @@ router.beforeEach(async (to: RouteLocationNormalized, from: RouteLocationNormali
       return { name: 'dashboard' }
     return true
   }
-  // console.log('--------------------------- Route ---------------------------')
-  // console.log({ to })
-  // console.log({ from })
   return true
 })
 
